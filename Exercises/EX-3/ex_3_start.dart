ec357a10a0a2c4b1426cbe77956e784595e1ae93
Lìlz ♡⚡︎ <liza@Lizas-Laptop.local>
2025-11-20 13:10:05 +0700

    Add EX-3 folder contents

--- Exercises/EX-3/ex_3_start.dart
+++ Exercises/EX-3/ex_3_start.dart
@@ -10,27 +10,55 @@ List<String> images = [
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false, // Why this line ? Can you explain it ?
-      home: Scaffold(
-        backgroundColor: Colors.green[50],
-        appBar: AppBar(
-          backgroundColor: Colors.green[400],
-          title: const Text('Image viewer'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.navigate_before),
-              tooltip: 'Go to the previous image',
-              onPressed: () => {},
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
-              child: IconButton(
-                icon: const Icon(Icons.navigate_next),
-                tooltip: 'Go to the next image',
-                onPressed: () => {},
-              ),
-            ),
-          ],
-        ),
-        body: Image.asset(images[0]),
-      ),
+      home: DisplayImage(),
     ));
+
+class DisplayImage extends StatefulWidget {
+
+  const DisplayImage({
+    super.key,
+  });
+
+  @override
+  State<DisplayImage> createState() => _DisplayImageState();
+}
+
+class _DisplayImageState extends State<DisplayImage> {
+  int currentImageIndex = 0;
+ void previousImage(){
+  setState(() {
+    currentImageIndex = (currentImageIndex - 1) % images.length;
+  });
+ }
+ void nextImage (){
+  setState(() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;  
+  });
+ }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: const Text('Image viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.navigate_before),
+            tooltip: 'Go to the previous image',
+            onPressed: previousImage,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to the next image',
+              onPressed: nextImage,
+            ),
+          ),
+        ],
+      ),
+      body: Image.asset(images[currentImageIndex]),
+    );
+  }
+}
